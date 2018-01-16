a52ed63c36a5a99c3fa56d86f3aee87bd281bc80
Dedyset <dedy.smkn2bjb@gmail.com>
2018-01-16 18:29:54 +0700

    deskripsi domba

--- domba.cpp
+++ domba.cpp
@@ -7,3 +7,12 @@ class domba{
 		string bobot;
 		string warnaBulu;
 };
+
+int main(){
+	domba dombaGarut,dombaBatur;
+	
+	dombaGarut.bobot = "30-60 kg ";
+	dombaBatur.bobot = "80-90 kg ";
+	
+	dombaGarut.warnaBulu = "Putih, Hitam, Cokelat ";
+	dombaBatur.warnaBulu = "Putih ";
