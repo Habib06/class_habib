6832df53b2f2a3540a32799bf07ce4cf331dd2e1
Dedyset <dedy.smkn2bjb@gmail.com>
2018-01-16 18:29:24 +0700

    clas domba

---
+++ Untitled1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class domba{
+	public:
+		string bobot;
+		string warnaBulu;
+};
+
+int main(){
+	domba dombaGarut,dombaBatur;
+	
+	dombaGarut.bobot = "30-60 kg ";
+	dombaBatur.bobot = "80-90 kg ";
+	
+	dombaGarut.warnaBulu = "Putih, Hitam, Cokelat ";
+	dombaBatur.warnaBulu = "Putih ";
+
+	cout <<"Domba Garut berwarna " <<(dombaGarut.warnaBulu);
+	cout <<"Domba Garut bobotnya mencapai " <<(dombaGarut.bobot)<<endl;
+	
+	cout <<"Domba Batur berwarna " <<(dombaBatur.warnaBulu);
+	cout <<"Domba Batur bobotnya mencapai " <<(dombaBatur.bobot);
+}
+
---
+++ domba.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class domba{
+	public:
+		string bobot;
+		string warnaBulu;
+};
